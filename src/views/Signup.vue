197f73cf47598ddb38489eb139199252f3d01c8b
Alexis <alexis9653@hotmail.fr>
2021-05-06 16:32:33 +0200

    -- signup -- mise en page

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,61 +1,74 @@
 <template>
-  <div class="">
-    <form class="form-signin" @submit="sendData" method="post">
-      <div class="form-group">
-        <label for="formGroupExampleInput">Username</label>
-        <input
-          v-model="username"
-          name="username"
-          type="text"
-          class="form-control"
-          placeholder="Username"
-        />
-      </div>
+  <div class="container">
+    <div class="card p-5">
+      <form class="form-signin" @submit="sendData" method="post">
+        <div class="row">
+          <div class="col">
+            <div class="form-group">
+              <label for="formGroupExampleInput">Username</label>
+              <input
+                v-model="username"
+                name="username"
+                type="text"
+                class="form-control"
+                placeholder="Username"
+              />
+            </div>
 
-      <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input
-          v-model="email"
-          type="email"
-          name="email"
-          class="form-control"
-          aria-describedby="emailHelp"
-          placeholder="Enter email"
-        />
-        <small id="emailHelp" class="form-text text-muted"
-          >We'll never share your email with anyone else.</small
-        >
-      </div>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email address</label>
+              <input
+                v-model="email"
+                type="email"
+                name="email"
+                class="form-control"
+                aria-describedby="emailHelp"
+                placeholder="Enter email"
+              />
+              <small id="emailHelp" class="form-text text-muted"
+                >We'll never share your email with anyone else.</small
+              >
+            </div>
 
-      <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <input
-          v-model="password"
-          type="password"
-          name="password"
-          class="form-control"
-          placeholder="Password"
-        />
-      </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Password</label>
+              <input
+                v-model="password"
+                type="password"
+                name="password"
+                class="form-control"
+                placeholder="Password"
+              />
+            </div>
+          </div>
+          <div class="col">
+            <div class="form-group">
+              <label for="inputAddress">Address</label>
+              <input
+                v-model="address"
+                type="text"
+                class="form-control"
+                name="address"
+                placeholder="1234 Main St"
+              />
+            </div>
 
-      <div class="form-group">
-        <label for="inputAddress">Address</label>
-        <input
-          v-model="address"
-          type="text"
-          class="form-control"
-          name="address"
-          placeholder="1234 Main St"
-        />
-      </div>
-
-      <div class="form-group">
-        <label for="phone">Enter your phone number:</label>
-        <input v-model="phone" type="text" class="form-control" name="phone" />
-      </div>
-
-      <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+            <div class="form-group">
+              <label for="phone">Enter your phone number:</label>
+              <input
+                v-model="phone"
+                type="text"
+                class="form-control"
+                name="phone"
+              />
+            </div>
+          </div>
+          <div class="py-3">
+            <button type="submit" class="btn btn-success">Submit</button>
+          </div>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 

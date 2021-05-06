925da3504b2434d07b96fe9709a0a9ee9355c20e
Alexis <alexis9653@hotmail.fr>
2021-05-06 16:26:25 +0200

    -- signup -- btn dans nav

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,8 +5,11 @@
         ><i class="bi-book" style="font-size: 1.5em"></i
       ></router-link>
       <form class="d-flex">
-        <router-link to="/buy" class="btn btn-outline-success" type="submit"
+        <router-link to="/buy" class="btn btn-outline-success mx-1" type="submit"
           ><i class="bi-cart" style="font-size: 1.5em"></i
+        ></router-link>
+        <router-link to="/sign-up" class="btn btn-outline-success mx-1" type="submit"
+          ><i class="bi-box-arrow-in-right" style="font-size: 1.5em"></i
         ></router-link>
       </form>
     </div>
